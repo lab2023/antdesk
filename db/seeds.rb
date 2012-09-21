711ad1210f1fdbd158beb390946b291c61c00c43
alikatircioglu <ali.katircio@gmail.com>
2012-09-21 14:18:05 +0300

    Issue # 40 is completed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Admin.create(name: "lab2023 Admin", email: "info@lab2023.com", password: "12341234")
