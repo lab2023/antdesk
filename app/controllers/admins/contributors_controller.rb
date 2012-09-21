f2c4378880199d63b11b8df82640d173f66520b7
Yunus ÖZCAN <yunus.ozcan@lab2023.com>
2012-09-21 12:33:12 +0300

    added back button and fix auto complete bug

--- app/controllers/admins/contributors_controller.rb
+++ app/controllers/admins/contributors_controller.rb
@@ -1,7 +1,7 @@
 class Admins::ContributorsController < Admins::ApplicationController
   def auto
       contributors = Contributor.order(:name).where("name Like ?", "%#{params[:search]}%")
-      render :json => contributors.map {|p| Hash[id: p.id, label: "#{p.email}-#{p.name}", name: p.name]}
+      render :json => contributors.map {|p| Hash[id: p.id, label: "#{p.name}", name: p.name]}
   end
   load_and_authorize_resource
   def show
