86eb57984994e8ce83a7f592a6c77e2ccbad2eb4
Muhammet DİLEK <dilekmuhammet@gmail.com>
2012-09-22 12:13:17 +0300

    deploy settings changed

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,9 @@
-root = "/home/deployer/apps/support/current"
+root = "/home/deployer/apps/antdesk/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.support.sock"
+listen "/tmp/unicorn.antdesk.sock"
 worker_processes 2
 timeout 30
