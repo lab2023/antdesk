4ce57cf13bba119a9763cdc8b8d542a9781393a9
Yunus ÖZCAN <yunus.ozcan@lab2023.com>
2012-09-26 17:00:15 +0300

    add categories.first breadcrumb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,7 @@ class ArticlesController < ApplicationController
   def show
     @article = @current_application.articles.active.find(params[:id])
     add_breadcrumb "Anasayfa", :root_path
+    add_breadcrumb @article.categories.first.name, category_path(@article.categories.first)
     add_breadcrumb @article.name, :article_path
     respond_with(@article)
   end
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,6 +3,7 @@ class VideosController < ApplicationController
     @video = @current_application.videos.active.find(params[:id])
     @video.provider = @video.provider.html_safe
     add_breadcrumb "Anasayfa", :root_path
+    add_breadcrumb @video.categories.first.name, category_path(@video.categories.first)
     add_breadcrumb @video.name, :video_path
     respond_with(@video)
   end
